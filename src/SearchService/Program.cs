bbdc31bf9bbf36c7cb6212e658546bd33f252e0c
pcandler <patcan_80@yahoo.com>
2024-05-17 19:08:20 -0400

    end of section 3

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,6 +1,7 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService;
+using SearchService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,13 +20,13 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-await DB.InitAsync("SearchDb", MongoClientSettings
-    .FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
-
-await DB.Index<Item>()
-    .Key(x => x.Make, KeyType.Text)
-    .Key(x => x.Model, KeyType.Text)
-    .Key(x => x.Color, KeyType.Text)
-    .CreateAsync();
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
     
 app.Run();
